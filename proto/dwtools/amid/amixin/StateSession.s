8af155acfde1bbe6bd76a0016ecc076d8d186785
wandalen <wandalen.me@gmail.com>
2020-06-09 21:33:33 +0300

    .

--- proto/dwtools/amid/amixin/StateSession.s
+++ proto/dwtools/amid/amixin/StateSession.s
@@ -33,7 +33,8 @@ if( typeof module !== 'undefined' )
 let _global = _global_;
 let _ = _global_.wTools;
 let Parent = null;
-let Self = function wStateSession( o )
+let Self = wStateSession;
+function wStateSession( o )
 {
   _.assert( arguments.length === 0 || arguments.length === 1, 'Expects single argument' );
   return _.workpiece.construct( Self, this, arguments );
